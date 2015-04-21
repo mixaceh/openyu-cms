efac93d8e17d93eb9aad11e8b1eae173b21979c5
mixaceh <mixaceh@gmail.com>
2015-04-21 15:02:03 +0800

    更改目錄

--- openyu-cms-core.j/src/test/java/org/openyu/cms/app/AppBeanTestSupporter.java
+++ openyu-cms-core.j/src/test/java/org/openyu/cms/app/AppBeanTestSupporter.java
@@ -8,23 +8,44 @@ public abstract class AppBeanTestSupporter extends BeanTestSupporter {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
 		// 在web下, 設定檔的目錄會放不同, 所以測試時會發生找不到檔案的狀況,因此單元測試中
 		// 更改以下設定的目錄
-		AppConfigHelper
-				.setConfigFile("src/main/webapp/WEB-INF/data/conf/config.xml");
-		AppConfigHelper.setJsonDir("src/main/webapp/WEB-INF/data/json");
-		AppConfigHelper.setKeyDir("src/main/webapp/WEB-INF/data/key");
-		AppConfigHelper.setSerDir("src/main/webapp/WEB-INF/data/ser");
-		AppConfigHelper.setXmlDir("src/main/webapp/WEB-INF/data/xml");
-		AppConfigHelper.setExcelDir("src/main/webapp/WEB-INF/data/excel");
+
+		// app
 		//
-		AppConfigHelper.setOutputDir("src/main/webapp/WEB-INF/data/output");
-		AppConfigHelper.setDownloadDir("src/main/webapp/WEB-INF/data/download");
-		AppConfigHelper.setUploadDir("src/main/webapp/WEB-INF/data/upload");
+		AppConfigHelper.setConfigFile("src/test/config/etc/config.xml");
+		AppConfigHelper.setJsonDir("src/test/config/json");
+		AppConfigHelper.setKeyDir("src/test/config/key");
+		AppConfigHelper.setSerDir("src/test/config/ser");
+		AppConfigHelper.setXmlDir("src/test/config/xml");
+		AppConfigHelper.setExcelDir("src/test/config/excel");
 		//
-		AppConfigHelper.setResourcePath("src/main/webapp/custom/resource");
-		AppConfigHelper
-				.setTemplatePath("src/main/webapp/WEB-INF/custom/template");
-		AppConfigHelper.setBackupPath("src/main/webapp/WEB-INF/backup");
+		AppConfigHelper.setOutputDir("custom/output");
+		AppConfigHelper.setDownloadDir("custom/download");
+		AppConfigHelper.setUploadDir("custom/upload");
+		//
+		AppConfigHelper.setResourcePath("custom/resource");
+		AppConfigHelper.setTemplatePath("custom/template");
+		AppConfigHelper.setBackupPath("custom/backup");
+
+		// web
+		//
+		// AppConfigHelper
+		// .setConfigFile("src/main/webapp/WEB-INF/config/etc/config.xml");
+		// AppConfigHelper.setJsonDir("src/main/webapp/WEB-INF/config/json");
+		// AppConfigHelper.setKeyDir("src/main/webapp/WEB-INF/config/key");
+		// AppConfigHelper.setSerDir("src/main/webapp/WEB-INF/config/ser");
+		// AppConfigHelper.setXmlDir("src/main/webapp/WEB-INF/config/xml");
+		// AppConfigHelper.setExcelDir("src/main/webapp/WEB-INF/config/excel");
+		// //
+		// AppConfigHelper.setOutputDir("src/main/webapp/custom/output");
+		// AppConfigHelper.setDownloadDir("src/main/webapp/custom/download");
+		// AppConfigHelper.setUploadDir("src/main/webapp/custom/upload");
+		// //
+		// AppConfigHelper.setResourcePath("src/main/webapp/custom/resource");
+		// AppConfigHelper
+		// .setTemplatePath("src/main/webapp/WEB-INF/custom/template");
+		// AppConfigHelper.setBackupPath("src/main/webapp/WEB-INF/custom/backup");
 	}
 }
